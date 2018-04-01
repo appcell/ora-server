d45dd1b4bd725607d3c2e90b7b140083cc9de1ae
Ran Xiao <genesisx000@gmail.com>
2018-04-01 14:29:40 -0400

    update checking and client download

---
+++ service/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, send_file
+app = Flask(__name__)
+
+import configparser
+config = configparser.ConfigParser()
+config.read('etc/conf.ini')
+
+@app.route("/api/check_update/<current>")
+def check_update(current):
+    latest_version = config['update']['client_version']
+    if current < latest_version:
+        return jsonify({'is_latest':False, 'new_version':latest_version})
+    else:
+        return jsonify({'is_latest':'True'})
+
+@app.route("/download/client/<path>")
+def client_download(path):
+    if path is None:
+        return 'invalid argument', 400
+    try:
+        return send_file('files/ora-{}.zip'.format(path), as_attachment=True)
+    except Exception as err:
+        return 'file not found', 404
