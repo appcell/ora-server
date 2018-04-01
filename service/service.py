20e7239a47ba30dd0f2e3d7fa97529e940f0fc6d
Ran Xiao <genesisx000@gmail.com>
2018-04-01 17:20:58 -0400

    uwsgi settings

--- service/service.py
+++ service/service.py
@@ -5,6 +5,10 @@ import configparser
 config = configparser.ConfigParser()
 config.read('etc/conf.ini')
 
+@app.route("/")
+def root():
+    return "Hello World!"
+
 @app.route("/api/check_update/<current>")
 def check_update(current):
     latest_version = config['update']['client_version']
@@ -21,3 +25,6 @@ def client_download(path):
         return send_file('files/ora-{}.zip'.format(path), as_attachment=True)
     except Exception as err:
         return 'file not found', 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
